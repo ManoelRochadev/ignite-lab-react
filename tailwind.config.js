c0efef033c326d43a98ac6a0acb39974568719d0
Manoel Rocha <manoelmelo367@gmail.com>
2022-07-10 13:38:14 +0000

    BUG image resolved && ADD config vercel

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        blur: 'url(/blur-bg.png)'
+        blur: 'url(/src/assets/blur-bg.png)'
       },
       fontFamily: {
         sans: 'Roboto, sans-serif',
